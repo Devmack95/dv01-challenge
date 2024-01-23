79abebf4242532652681154a9eb07790e9573c18
Devmack95 <devon.mccalmon@yahoo.com>
2024-01-22 19:19:24 -0500

    import data into app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
+import { getData } from './request/api'
 
 class App extends Component {
+  
+  async componentDidMount() {
+    try {
+      const data = await getData();
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   render() {
     return (
       <div className="App">
